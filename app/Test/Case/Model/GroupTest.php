1e531607ba8f2a003752f821d91dcdb3e7e65604
Janne Kallunki <janne@kallunki.org>
2012-11-13 22:30:59 +0200

    korjauksia testicaseen

--- app/Test/Case/Model/GroupTest.php
+++ app/Test/Case/Model/GroupTest.php
@@ -12,28 +12,38 @@ class GroupTest extends CakeTestCase {
 	public function testGroups() {
 		$result = $this->Group->groups();
 		$expected = array(
-			array('Group' => array('id' => 1, 'course_id' => 1, 'user_id' => 2, 
-				array('User' => array(
+			array(
+				'Group' => array(
+					'id' => 1, 
+					'course_id' => 1, 
+					'user_id' => 2
+				),
+				'User' => array(
 					'id' => 2, 
-					'basic_user_account' => 23456,
+					'basic_user_account' => '23456',
 					'last_name' => 'Assistentti',
 					'first_name' => 'Asseri',
 					'email' => 'asseri.assistentti@uta.fi',
-					'is_admin' => 'false',
-					'password' => 'testi')
-					)
+					'is_admin' => false,
+					'password' => 'testi',
+					'name' => 'Asseri Assistentti'
 				)
 			),
-			array('Group' => array('id' => 2, 'course_id' => 1, 'user_id' => 3, 
-				array('User' => array(
+			array(
+				'Group' => array(
+					'id' => 2, 
+					'course_id' => 1, 
+					'user_id' => 3
+				),
+				'User' => array(
 					'id' => 3, 
-					'basic_user_account' => 34567,
+					'basic_user_account' => '34567',
 					'last_name' => 'Assistentti',
 					'first_name' => 'Testi',
 					'email' => 'testi.assistentti@uta.fi',
-					'is_admin' => 'false',
-					'password' => 'testi')
-					)
+					'is_admin' => false,
+					'password' => 'testi',
+					'name' => 'Testi Assistentti'
 				)
 			)	 
 		);
