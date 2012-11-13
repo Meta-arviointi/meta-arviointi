c09030fb9ce53b7629763ff7de73443a68b602ff
Janne Kallunki <janne@kallunki.org>
2012-11-13 22:58:45 +0200

    testi Student modelille

---
+++ app/Test/Fixture/StudentFixture.php
@@ -0,0 +1,19 @@
+<?php
+
+class StudentFixture extends CakeTestFixture {
+	/* Import data from production database */
+	public $import = array('model' => 'Student');
+
+	public $records = array(
+		array(
+			'id' => 1,
+			'student_number' => 12345, 
+			'last_name' => 'Teppo', 
+			'first_name' => 'Testilä',
+			'email' => 'teppo.testila@uta.fi', 
+			'group_id' => '1'
+		)
+	);
+}
+
+?>
