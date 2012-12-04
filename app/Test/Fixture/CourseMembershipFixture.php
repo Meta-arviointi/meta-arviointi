0d013adaec2daea9e1a7e6ee919d5c2a5897ccf0
Janne Kallunki <janne@kallunki.org>
2012-12-04 16:04:57 +0200

    fixture for coursemembership

---
+++ app/Test/Fixture/CourseMembershipFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+class CourseMembershipFixture extends CakeTestFixture {
+	/* Import data from production database */
+	public $import = array('model' => 'CourseMembership');
+
+	public $records = array(
+	);
+}
+
+?>
