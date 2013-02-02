ef169a8acc2141d94ca7bc1256fbf9f9719c143d
Snurppa <snurppa@gmail.com>
2013-02-02 21:09:23 +0200

    Embarrasing bux fixed when saving actions...

--- app/Controller/ActionsController.php
+++ app/Controller/ActionsController.php
@@ -9,19 +9,23 @@ class ActionsController extends AppController {
 
             // Convert date and time from Datepicker to match database timestamp format
             // ie. '06.02.2013 00:15' converts to '2013-02-06 00:15:00+0200'
-            $deadline_date = $this->request->data['Action']['deadline_date'];
-            $deadline_time = $this->request->data['Action']['deadline_time'];
+            if ( $this->request->data['Action']['deadline_date'] &&
+                $this->request->data['Action']['deadline_time'] ) {
 
-            // 'dd.mm.yyyy hh:mm'
-            $deadline_string = $deadline_date . ' ' . $deadline_time['hour'] . ':' . $deadline_time['min'];
+                $deadline_date = $this->request->data['Action']['deadline_date'];
+                $deadline_time = $this->request->data['Action']['deadline_time'];
 
-            $deadline = date_create_from_format('d.m.Y H:i', $deadline_string);
-            $deadline_dbstring = date_format($deadline, 'Y-m-d H:i:sO');
+                // 'dd.mm.yyyy hh:mm'
+                $deadline_string = $deadline_date . ' ' . $deadline_time['hour'] . ':' . $deadline_time['min'];
 
-            // Set value for saving and unset unnecessary variables
-            $this->request->data['Action']['deadline'] = $deadline_dbstring;
-            unset($this->request->data['Action']['deadline_date']);
-            unset($this->request->data['Action']['deadline_time']);
+                $deadline = date_create_from_format('d.m.Y H:i', $deadline_string);
+                $deadline_dbstring = date_format($deadline, 'Y-m-d H:i:sO');
+
+                // Set value for saving and unset unnecessary variables
+                $this->request->data['Action']['deadline'] = $deadline_dbstring;
+                unset($this->request->data['Action']['deadline_date']);
+                unset($this->request->data['Action']['deadline_time']);
+            }
 
             if($this->Action->save($this->request->data)) {
                 // Get ID of new saved Action
